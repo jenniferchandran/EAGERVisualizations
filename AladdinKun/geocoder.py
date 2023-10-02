c7cd2743b7e543e004b8aa6c0bd8440e1754fb80
Kun Alading <alankent911@gmail.com>
2023-10-02 00:55:25 -0400

    10/02/2023 Aladdin Kun

---
+++ AladdinKun/geocoder.py
@@ -0,0 +1,64 @@
+import geopandas as gpd
+import cartopy.io.shapereader as shpreader
+import pandas as pd
+from shapely.geometry import Point, shape
+from geopy.geocoders import Nominatim
+import pandas
+
+# Mapping of state names to their abbreviations for the US
+state_to_abbr = {
+    'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA', 'Colorado': 'CO',
+    'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI', 'Idaho': 'ID',
+    'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA', 'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA',
+    'Maine': 'ME', 'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS',
+    'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+    'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH',
+    'Oklahoma': 'OK', 'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC',
+    'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT', 'Virginia': 'VA',
+    'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY'
+}
+
+# Function to get the state from coordinates
+
+
+def get_state_from_coords(arg):
+    Latitude, Longitude = arg
+    geolocator = Nominatim(user_agent="myGeocodeApp")
+
+    try:
+        location = geolocator.reverse(
+            (float(Latitude), float(Longitude)), exactly_one=True)
+        address = location.raw['address']
+        state = address.get('state', "")
+        return state_to_abbr.get(state, state)
+    except:
+        return pandas.NA
+
+
+# Get the geometries of US states
+reader = shpreader.Reader(shpreader.natural_earth(
+    resolution='110m', category='cultural', name='admin_1_states_provinces'))
+states = [state for state in reader.records(
+) if state.attributes['iso_a2'] == 'US']
+
+# Convert these states to a GeoDataFrame
+data = {
+    "name": [state.attributes['name'] for state in states],
+    "geometry": [state.geometry for state in states]
+}
+gdf_states = gpd.GeoDataFrame(data)
+
+
+def get_state_abbr_from_coords(arg):
+    latitude, longitude = arg
+    geom = [Point(longitude, latitude)]
+    point_gdf = gpd.GeoDataFrame(geometry=geom)
+
+    # Spatial join between point and states to find out which state it belongs to
+    point_in_states = gpd.sjoin(
+        point_gdf, gdf_states, how="inner", op="within")
+    if point_in_states.empty:
+        return pd.NA
+    else:
+        state_name = point_in_states.iloc[0]['name']
+        return state_to_abbr.get(state_name, state_name)
