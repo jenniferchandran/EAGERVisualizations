86fb9ff3bac02dae802a3ee1b853b2221e4279d0
JusticeJJackson <JusticeJackson@vt.edu>
2023-10-05 13:34:17 -0400

    updated some things

--- justiceWeather/visualizations/jsViz/app.jsx
+++ justiceWeather/visualizations/jsViz/app.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Map } from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
 import DeckGL from '@deck.gl/react';
-import { ScatterplotLayer } from '@deck.gl/layers';
+import { GridLayer } from '@deck.gl/aggregation-layers';
 
 const MALE_COLOR = [0, 128, 255];
 const FEMALE_COLOR = [255, 0, 128];
 
-// Source data CSV
-const DATA_URL =
-    'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/scatterplot/manhattan.json'; // eslint-disable-line
+const DATA_URL = 'http://127.0.0.1:5000/get_data';
 
 const INITIAL_VIEW_STATE = {
     longitude: -74,
@@ -22,20 +20,40 @@ const INITIAL_VIEW_STATE = {
 };
 
 export default function App({
-    data = DATA_URL,
     radius = 30,
     maleColor = MALE_COLOR,
     femaleColor = FEMALE_COLOR,
     mapStyle = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json'
 }) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Fetch data when the component mounts
+        const fetchData = async () => {
+            try {
+                console.log("fetching data");
+                const response = await fetch(DATA_URL);
+                console.log('resonse codde: ', response.status);
+                console.log("response: ", response);
+                const jsonData = await response.json();
+                console.log("jsonData: ", jsonData);
+                setData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependency array ensures that this effect runs once on mount
+
     const layers = [
-        new ScatterplotLayer({
-            id: 'scatter-plot',
+        new GridLayer({
+            id: 'grid-of-emotions',
             data,
             radiusScale: radius,
             radiusMinPixels: 0.25,
-            getPosition: d => [d[0], d[1], 0],
-            getFillColor: d => (d[2] === 1 ? maleColor : femaleColor),
+            getPosition: d => [d["Latitude"], d["Longitude"], 0],
+            getFillColor: d => (d['color']),
             getRadius: 1,
             updateTriggers: {
                 getFillColor: [maleColor, femaleColor]
